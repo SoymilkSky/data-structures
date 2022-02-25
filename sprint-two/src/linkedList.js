0dcf55f7bc78d51944d85982ad45c3fb86eca152
DannyWong <danny.96.wong@gmail.com>
2022-02-25 15:16:10 -0800

    finished passing tests for linkedlist and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,65 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
+  // takes a value, adds it to the end of the list
+  // create a new node with passed in value
+  // somehow find the end of the list?
+  // set the current tail.next -> to be the new node
+  // set list.tail to be new node
   list.addToTail = function(value) {
+    // delcare a new Node with passed in value
+    var newNode = new Node(value);
+
+    // check if head is null
+    // if it is, head = newNode
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = list.head;
+    } else {
+      var currNode = list.head;
+      // traverse the list to get to the tail
+      // check if current node.next === null
+      // if yes -> current node.next = newNode
+      while (currNode.next !== null) {
+        currNode = currNode.next;
+      }
+      currNode.next = newNode;
+      list.tail = newNode;
+
+      // for (var key in list)
+      // if list[key].next === null
+    }
   };
 
   list.removeHead = function() {
+    // set head.next to be new head
+    var nextHead = list.head.next;
+    var oldHead = list.head;
+    list.head = nextHead;
+    return oldHead.value;
   };
 
+
+  // IF WE HAVE TIME, MAKE THIS BETTER .
   list.contains = function(target) {
+    // same traversal as addToTail but check target vs currNode value
+    var currNode = list.head;
+    console.log(currNode.value);
+    while (currNode.next !== null) {
+      if (currNode.value === target) {
+        return true;
+      }
+      currNode = currNode.next;
+      // check if currNode.next is the tail, if it is, check value against target
+      if (currNode.next === null) {
+        if (currNode.value === target) {
+          return true;
+        }
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
@@ -26,4 +76,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail = O(1)
+ * removeHead = O(1)
+ * contains = O(n)
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fixED
+
+  // extend methods into function
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +14,80 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // declare new child object with passed in value
+  var newObj = Tree(value);
+  this.children.push(newObj);
+  // check if newTree.children is empty , if yes, we can just push new child object into
+  // newTree.children
 };
 
 treeMethods.contains = function(target) {
+  // what do you want to to for the current input
+  // case 1
+  // if the children.value is target, return true
+  if (this.value === target) {
+    return true;
+  }
+  // case 2
+  // case ..._
+  // detect nestedness
+  if (this.children.length !== 0) {
+    //if children is not empty, iterate through children array
+
+    for (var i = 0; i < this.children.length; i++) {
+      // iterate over nested structure
+      // recursively call function on each value
+
+      // danny's comment while looking at this during lunch
+      // I think we can use a conditional here,
+      //if this call below returns true, we can just return true ?
+      if (this.children[i].contains(target) === true) {
+        return true;
+      }
+    }
+  }
+  return false;
+
+  // --------- what I was thinking of (danny) --------
+  // if (this.children.value === target) {
+  //   return true;
+  // }
+
+  // var loopThroughChildren = function(child, targVal) {
+  //   if (child.children.length !== 0) {
+  //     for (var i = 0; i < child.children.length; i++) {
+  //       if (child[i].children.length !== 0) {
+  //         loopThroughChildren(child[i].children, targVal);
+  //       }
+  //     }
+  //   }
+  // };
+
+  // loopThroughChildren(this.children, target);
+
+  // return false;
+
 };
 
-
+// var newTree = new Tree();
+// newTree.addChild(5);
+// console.log(newTree);
+// console.log(newTree.children);
+// newTree.children[0].addChild(6);
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
  */
+
+// {
+//   value:
+//   children: [
+//     {value:
+//      children:
+//     },
+//     {value:
+//       children:
+//     }
+//   ]
+// }
