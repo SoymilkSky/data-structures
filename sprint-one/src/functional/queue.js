09290374f266ee72b7780d4e09de01eb72842711
DannyWong <danny.96.wong@gmail.com>
2022-02-25 21:51:31 -0800

    thought about fixing my functions to not overflow if count ever got too big(attempted)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,22 +4,33 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var index = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    if (Object.keys(storage).length === 0) {
-      storage[0] = value;
-    } else {
-      let keys = Object.keys(storage);
-      let newKey = keys[keys.length - 1] + 1;
-      storage[newKey] = value;
-    }
+    // if (Object.keys(storage).length === 0) {
+    //   storage[0] = value;
+    // } else {
+    //   let keys = Object.keys(storage);
+    //   let newKey = keys[keys.length - 1] + 1;
+    //   storage[newKey] = value;
+    // }
+
+    // refactoring so if count went to infinity, it would not cause an overflow
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
-    let keys = Object.keys(storage);
-    let dequeued = storage[keys[0]];
-    delete storage[keys[0]];
+    // let keys = Object.keys(storage);
+    // let dequeued = storage[keys[0]];
+    // delete storage[keys[0]];
+    // return dequeued;
+
+    //refactoring so if count went to infinity, it would not cause an overflow
+    let dequeued = storage[Object.keys(storage)[0]];
+    delete storage[Object.keys(storage)[0]];
     return dequeued;
   };
 
